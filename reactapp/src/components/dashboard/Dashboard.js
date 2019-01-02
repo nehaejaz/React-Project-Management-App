6c618d47895704d631a7eb71539e0d7be8a5e988
nehaejaz <neha.ejaz@icloud.com>
2019-01-03 02:14:06 +0500

    Route guarding done

--- reactapp/src/components/dashboard/Dashboard.js
+++ reactapp/src/components/dashboard/Dashboard.js
@@ -4,11 +4,13 @@ import  ProjectList  from "../project/ProjectList";
 import {connect} from 'react-redux';
 import {firestoreConnect} from 'react-redux-firebase'
 import {compose} from 'redux'
+import {Redirect} from 'react-router-dom'
 
 class Dashboard extends Component{
     render(){
         // console.log(this.props);
-        const {projects}=this.props;
+        const {projects, auth}=this.props;
+        if (!auth.uid) return <Redirect to='/signin'></Redirect>
         return(
             <div className="Dashboard container">
                 <div className="row">
@@ -27,7 +29,8 @@ class Dashboard extends Component{
 const mapStateToProps = (state) =>{
     console.log(state)
     return{
-        projects: state.firestore.ordered.projects
+        projects: state.firestore.ordered.projects,
+        auth: state.firebase.auth
     }
 }
 export default compose(
