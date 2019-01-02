8eb03df5054ddd9693ca4183fb83c2c41d8924ed
nehaejaz <neha.ejaz@icloud.com>
2019-01-02 23:56:07 +0500

    created firebase auth

--- reactapp/src/store/reducers/authReducer.js
+++ reactapp/src/store/reducers/authReducer.js
@@ -1,9 +1,33 @@
-const initState={
+import {
+    stat
+} from "fs";
+
+const initState = {
+
+    authError: null,
 
 }
 
-const authReducer= (state= initState,action)=>{
+const authReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'Login_Error':
+            console.log('Login Error')
+            return {
+                ...state,
+                authError: 'Login Failed'
+            }
+        case 'Login_Success':
+            console.log('login_Success');
+            return {
+                ...state,
+                authError: null
+            }
+        case 'SignOut_Success':
+            console.log("Signed out successfully");
+            return state
+        default:
+            return state;
+    }
 
-    return(state) 
 }
 export default authReducer
