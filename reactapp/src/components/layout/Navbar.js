65ff68f49588d0cc32245d43bfeb6ca2c2b16849
nehaejaz <neha.ejaz@icloud.com>
2019-01-03 22:38:19 +0500

    passed profile property as props

--- reactapp/src/components/layout/Navbar.js
+++ reactapp/src/components/layout/Navbar.js
@@ -3,11 +3,12 @@ import {Link} from 'react-router-dom';
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import { connect} from "react-redux";
+import { stat } from 'fs';
 
 const Navbar =(props)=>{
-    const {auth} =props;
+    const {auth, profile} =props;
     console.log(auth);
-    const links = auth.uid ? <SignedInLinks/> : <SignedOutLinks />
+    const links = auth.uid ? <SignedInLinks profile={profile}/> : <SignedOutLinks />
     return(
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
@@ -20,7 +21,8 @@ const Navbar =(props)=>{
 const mapStateToProps=(state)=>{
     console.log(state);
     return{
-         auth: state.firebase.auth
+         auth: state.firebase.auth,
+         profile: state.firebase.profile
     }
 }
 export default connect(mapStateToProps)(Navbar)
