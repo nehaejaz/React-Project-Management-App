ede0c3cfe47ac13c7ff648b9fddd546926f36be8
nehaejaz <neha.ejaz@icloud.com>
2019-01-21 20:38:15 +0500

    passed notification as props and showed them through maps

--- reactapp/src/components/dashboard/Notifications.js
+++ reactapp/src/components/dashboard/Notifications.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from 'react';
+import momemt from 'moment'
 
 const Notifications=(props)=>{
+    const {notifications}= props
     return(
         <div className="section">
         <div className="card z-depth-0">
         <div className="card-content">
         <span className="card-titile">Notifications</span>
         <ul className="Notifications">
-        <li>Notification</li>
-        <li>Notification</li>
-        <li>Notification</li>
-        <li>Notification</li>
+        {notifications && notifications.map(item => {
+            return(
+            <li key={item.id}>
+            <span className="pink-text">{item.user}</span>
+            <span className="blue=text"> {item.content}</span>
+            <div className="grey-text note-date">
+            {momemt(item.time.toDate()).fromNow()}
+            </div>
+            </li>)
+        })}
         </ul>
         </div>
         </div>   
